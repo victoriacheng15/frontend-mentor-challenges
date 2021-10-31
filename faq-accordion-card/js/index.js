4bccbd9a84b537e71c3f23e36b8e531724e2f645
Victoria Cheng <victoria.cheng15@gmail.com>
2021-10-31 17:17:29 -0400

    add user interaction with arrow down image

--- faq-accordion-card/js/index.js
+++ faq-accordion-card/js/index.js
@@ -1,29 +1,13 @@
-import { questions } from './questions.js'
+import { createFaqCard } from './questions.js'
 
-const faqDiv = document.querySelector('.questions-answers')
+const questionSections = document.querySelectorAll('.question-section')
 
-// functions
-const createNewElement = element => document.createElement(element)
-const addClass = (element, className) => element.classList.add(className)
-const addTextContent = (element, text) => (element.textContent = text)
+questionSections.forEach(questionSection => {
+  const arrowDownBtn = questionSection.querySelector('.arrow-down')
+  const showAnswer = questionSection.querySelector('.answer-text')
 
-const createFaqCard = (() => {
-  // map out questions from questions.js
-  questions.map(questionList => {
-    const section = createNewElement('section')
-    addClass(section, 'question-section')
-
-    const questionTitle = createNewElement('h3')
-    addClass(questionTitle, 'question-title')
-    addTextContent(questionTitle, questionList.question)
-
-    const answerText = createNewElement('p')
-    addClass(answerText, 'answer-text')
-    addTextContent(answerText, questionList.answer)
-
-    const addSections = [questionTitle, answerText]
-    addSections.map(addSection => section.appendChild(addSection))
-
-    faqDiv.appendChild(section)
+  arrowDownBtn.addEventListener('click', () => {
+    showAnswer.classList.toggle('active')
+    console.log('you clicked')
   })
-})()
+})
--- faq-accordion-card/js/questions.js
+++ faq-accordion-card/js/questions.js
@@ -1,4 +1,4 @@
-export const questions = [
+const questions = [
   {
     question: 'How many team members can I invite?',
     answer:
@@ -23,3 +23,40 @@ export const questions = [
       'Chat and email support is available 24/7. Phone lines are open during normal business hours.',
   },
 ]
+
+const faqDiv = document.querySelector('.questions-answers')
+
+// functions
+const createNewElement = element => document.createElement(element)
+const addClass = (element, className) => element.classList.add(className)
+const addTextContent = (element, text) => (element.textContent = text)
+
+const createFaqCard = (() => {
+  const imageSrc = './images/icon-arrow-down.svg'
+  // map out questions and answers from questions.js
+  questions.map(questionList => {
+    const section = createNewElement('section')
+    addClass(section, 'question-section')
+
+    const questionTitle = createNewElement('h3')
+    addClass(questionTitle, 'question-title')
+    addTextContent(questionTitle, questionList.question)
+
+    const arrow = createNewElement('img')
+    addClass(arrow, 'arrow-down')
+    arrow.src = imageSrc
+    arrow.alt = 'arrow down'
+    questionTitle.appendChild(arrow)
+
+    const answerText = createNewElement('p')
+    addClass(answerText, 'answer-text')
+    addTextContent(answerText, questionList.answer)
+
+    const addSections = [questionTitle, answerText]
+    addSections.map(addSection => section.appendChild(addSection))
+
+    faqDiv.appendChild(section)
+  })
+})()
+
+export { createFaqCard }
