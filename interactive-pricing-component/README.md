17b441f63cc1414e24b1a8279737cb328495b0c0
Victoria Cheng <victoria.cheng15@gmail.com>
2021-12-09 18:43:01 -0500

    add files for interactive price componment

---
+++ interactive-pricing-component/README.md
@@ -0,0 +1,36 @@
+# Interactive pricing component solution
+
+The challenge is to build out this [interatice price component](https://www.frontendmentor.io/challenges/interactive-pricing-component-t0m8PIyY8) and get it looking as close to the design as possible.
+
+The goals:
+- To strengthen my skills on SCSS and JavaScript
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Use the slider and toggle to see prices for different page view numbers
+
+## Screenshot
+
+![interactive-price-component](https://user-images.githubusercontent.com/35031228/145492733-e2b077ce-d643-4b5b-8c02-5ea267698319.png)
+
+
+## Links
+
+- [Frontend Mentor Solution]()
+- [Github Live Page]()
+
+
+## Built with
+
+- Semantic HTML5 markup
+- SCSS
+- Mobile-first workflow
+- JavaScript
+
+## Author
+
+- Frontend Mentor - [@victoriacheng15](https://www.frontendmentor.io/profile/victoriacheng15)
+- Github - [@victoriacheng15](https://github.com/victoriacheng15)
+
+## Acknowledgments
+
+As always, thanks to people at Frontend Mentor for offering the oportunity to allow me practice my skills.
---
+++ interactive-pricing-component/js/script.js
@@ -0,0 +1,72 @@
+const rangeInput = document.querySelector('input[type="range"]');
+const costs = document.querySelectorAll('[data-cost]');
+const pageviews = document.querySelector('[data-pageviews]');
+const discountToggle = document.querySelector('[data-discount] input');
+const span = document.querySelector('[data-discount] span');
+
+let monthly;
+let views;
+
+let infos = [
+  {price: 8, views: '10K'},
+  {price: 12, views: '50K'},
+  {price: 16, views: '100K'},
+  {price: 24, views: '500K'},
+  {price: 32, views: '1M'},
+]
+
+// const views = ['10K', '50K', '100K', '500K', '1M']
+
+function handleDiscount() {
+  span.classList.toggle('discount');
+
+  updateValues();
+}
+
+
+function updateValues() {
+  let max = this.max;
+  let min = this.min;
+  let value = this.value;
+
+  bgSize = Math.floor(((value - min) * 100) / (max - min));
+  rangeInput.style.backgroundSize = `${bgSize}% 100%`;
+
+  const valueRange = {
+    price8: bgSize <= 20, // 10k pageviews
+    price12: bgSize > 20 && bgSize <= 40, // 50k pageviews
+    price16: bgSize > 40 && bgSize <= 60, // 100k pageviews
+    price24: bgSize > 60 && bgSize <= 80, // 500k pageviews
+    price36: bgSize > 80 && bgSize <= 100, // 1M pageviews
+  };
+
+  const { price8, price12, price16, price24, price36 } = valueRange;
+
+  const priceAndView = (index) => {
+    monthly = infos[index].price;
+    views = infos[index].views;
+  }
+
+  if (price8) {
+    priceAndView(0)
+  } else if (price12) {
+    priceAndView(1)
+  } else if (price16) {
+    priceAndView(2)
+  } else if (price24) {
+    priceAndView(3)
+  } else if (price36) {
+    priceAndView(4)
+  }
+
+  if (discountToggle.checked) {
+    monthly = monthly * 0.75
+  }
+
+  costs.forEach((cost) => (cost.textContent = `${monthly}.00`));
+  pageviews.textContent = views;
+}
+
+
+discountToggle.addEventListener('click', handleDiscount);
+rangeInput.addEventListener('input', updateValues);
